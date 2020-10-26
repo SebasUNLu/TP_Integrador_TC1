1f8d7a8fb5cdc62c7d0b5fdf41e9043e5740eb2f
FacundoWilliams <45250454+FacundoWilliams@users.noreply.github.com>
2020-10-26 04:36:22 -0300

    Clase para leer y escribir en archivos la lista Token

--- integrador/src/integrador/TokenObject.java
+++ integrador/src/integrador/TokenObject.java
@@ -1,6 +1,8 @@
 package integrador;
 
-public class TokenObject {
+import java.io.Serializable;
+
+public class TokenObject implements Serializable {
 	
 	private String nombre;
 	private String token;
---
+++ integrador/src/integrador/inputOutputFile.java
@@ -0,0 +1,56 @@
+package integrador;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class inputOutputFile implements Serializable {
+	
+	void guardarArchivo(TokenObject token) {
+		try (FileOutputStream fos = new FileOutputStream("tablaSimbolos.txt");
+			     ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+
+			    // Escribe el token en el archivo
+			    oos.writeObject(token);
+
+			} catch (IOException ex) {
+			    ex.printStackTrace();
+			}
+	}
+	
+	void leerArchivo (TokenObject token) {
+		
+		try
+	    {    
+	        // Leyendo del archivo 
+	        FileInputStream file = new FileInputStream("tablaSimbolos.txt"); 
+	        ObjectInputStream in = new ObjectInputStream(file); 
+	          
+	        // des serializaicon del token 
+	        token = (TokenObject)in.readObject(); 
+	          
+	        in.close(); 
+	        file.close(); 
+	          
+	        System.out.println("Tabla de Simbolos"); 
+	        System.out.println(token); 
+	    } 
+	      
+	    catch(IOException ex) 
+	    { 
+	        System.out.println("IOException is caught"); 
+	    } 
+	      
+	    catch(ClassNotFoundException ex) 
+	    { 
+	        System.out.println("ClassNotFoundException is caught"); 
+	    } 
+		
+
+	
+
+} 
+}
